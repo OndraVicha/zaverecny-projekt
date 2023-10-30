dd63e5714587c7c68fa85b90bf31d98dfbef0635
OndraVicha <77784807+OndraVicha@users.noreply.github.com>
2023-10-30 23:32:58 +0100

    Delete Web directory

--- Web/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Model3D, Image2D, Comment, Rating
-
-admin.site.register(Model3D)
-admin.site.register(Image2D)
-admin.site.register(Comment)
-admin.site.register(Rating)
--- Web/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Model3D
-
-class ModelUploadForm(forms.ModelForm):
-    class Meta:
-        model = Model3D
-        fields = ['title', 'description', 'file']
